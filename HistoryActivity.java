5182dd4bffb9b221ab0964530efc1099ac09d858
101442502 <101442502@georgebrown.ca>
2025-10-30 10:46:41 -0400

    Update HistoryActivity.java

--- HistoryActivity.java
+++ HistoryActivity.java
@@ -1,28 +1,36 @@
 package com.midterm.sueda;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.ArrayAdapter;
+import java.util.ArrayList;
 
 public class HistoryActivity extends AppCompatActivity {
 
-    ListView historyListView;
+    ListView listView;
+    ArrayList<String> tableList = new ArrayList<>();
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_history);
+        setContentView(R.layout.activity_main);
 
-        historyListView = findViewById(R.id.historyList);
+        listView = findViewById(R.id.listView);
 
-        ArrayAdapter<Integer> adapter = new ArrayAdapter<>(
-                this,
-                android.R.layout.simple_list_item_1,
-                MainActivity.historyList
-        );
+        String number = getIntent().getStringExtra("number");
 
-        historyListView.setAdapter(adapter);
+        if (number != null && !number.isEmpty()) {
+            int n = Integer.parseInt(number);
+
+            for (int i = 1; i <= 10; i++) {
+                tableList.add(n + " x " + i + " = " + (n * i));
+            }
+        }
+
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, tableList);
+        listView.setAdapter(adapter);
     }
 }
+
